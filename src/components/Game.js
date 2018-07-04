7fded9a80c9d490ecc9217a2a09fbb2bb1ec2776
Rabin Chaudhary <nibar18@gmail.com>
2018-07-05 02:02:31 +1000

    Added random number and associated sum numbers

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import PropTypes from 'prop-types';
 
 class Game extends React.Component{
-  target= 10 + Math.floor(40* Math.random());
+
+  static propTypes ={
+    randomNumberCount:PropTypes.number.isRequired,
+  }
+
+
+
+  randomNumbers = Array.from({length:this.props.randomNumberCount})
+  .map(()=>1+ Math.floor(10* Math.random()));
+
+
+  target= this.randomNumbers.slice(0,this.props.randomNumberCount-2).reduce((acc,curr)=>acc +curr,0);
+//// TODO: shuffle the random numbers
+
   render(){
     return(
       <View style={styles.container}>
         <Text style={styles.target}>{this.target}</Text>
+        <View style={styles.elementContainer} >
+        {this.randomNumbers.map((randomNumber,index)=>
+          <Text style={styles.elements} key={index}>{randomNumber}</Text>
+        )}
+        </View>
       </View>
-    )
+    );
   }
 }
 const styles= StyleSheet.create({
@@ -18,12 +37,28 @@ const styles= StyleSheet.create({
     paddingTop:30,
   },
   target:{
-    fontSize:40,
-    backgroundColor:'#aaa',
-    marginHorizontal:50,
+    fontSize:50,
+    backgroundColor:'#bbb',
+    margin:50,
     textAlign:'center',
+  },
+  elementContainer:{
+  flex:1,
+  flexDirection:'row',
+  flexWrap:'wrap',
+  justifyContent:'space-around',
 
   },
+  elements:{
+  backgroundColor:'#999',
+  width:100,
+  marginHorizontal:15,
+  marginVertical:25,
+  fontSize:25,
+  textAlign:'center',
+
+  }
+
 
 });
 
