89066b8d488a147469bc54d4126274bfa4edf212
Rabin Chaudhary <nibar18@gmail.com>
2018-07-05 02:49:59 +1000

    Added logic to change UI of numbers after pressed

---
+++ src/components/RandomNumber.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {View, Text, StyleSheet,TouchableOpacity} from 'react-native';
+import PropTypes from 'prop-types';
+
+class RandomNumber extends React.Component{
+  static propTypes={
+    number:PropTypes.number.isRequired,
+    isSelected:PropTypes.bool.isRequired,
+  }
+  pressEventHandler =()=>{
+    console.log(this.props.number);
+
+  }
+  render(){
+    return(
+      <TouchableOpacity
+        onPress={this.pressEventHandler}>
+      <Text style={[styles.elements, this.props.isSelected && styles.selected]}>{this.props.number}</Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+
+  elements:{
+  backgroundColor:'#999',
+  width:100,
+  marginHorizontal:15,
+  marginVertical:25,
+  fontSize:35,
+  textAlign:'center',
+},
+selected:{
+  opacity:0.3,
+}
+
+});
+export default RandomNumber;
